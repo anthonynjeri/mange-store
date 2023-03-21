dcac1428fe36d1b87dbc54522b683e798d164fae
anthonynwaweru <anthonypmarcel@gmail.com>
2023-03-21 11:42:51 +0300

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders start shopping link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/start shopping/i);
   expect(linkElement).toBeInTheDocument();
 });
